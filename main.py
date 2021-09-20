ba6f09fb16c0e147dcb6a3b56992669815952e3c
Sunny Cheung <sunnycheungabc@gmail.com>
2021-09-20 18:26:30 +0800

    rename background to road

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from constants import *
 
 assets_manager = AssetsManager()
 
-class Background(pygame.sprite.Sprite):
+class Road(pygame.sprite.Sprite):
     def __init__(self, image: pygame.Surface, x: int, y: int):
         pygame.sprite.Sprite.__init__(self)
 
@@ -16,7 +16,7 @@ class Background(pygame.sprite.Sprite):
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
-    def update(self, dx, dy):
+    def update(self, dx: int, dy: int):
         self.rect.left += dx
         if self.rect.left > self.width:
             self.rect.left -= self.width * 2
@@ -53,10 +53,12 @@ def main():
 
     window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    assets_manager.images['road'] = pygame.transform.scale(assets_manager.images['road'], (SCREEN_WIDTH, SCREEN_HEIGHT))
-    backgrounds = pygame.sprite.Group(
-        Background(assets_manager.images['road'], 0, 0),
-        Background(assets_manager.images['road'], SCREEN_WIDTH, 0),
+    assets_manager.images['road'] = pygame.transform.scale(
+        assets_manager.images['road'], (SCREEN_WIDTH, SCREEN_HEIGHT)
+    )
+    roads = pygame.sprite.Group(
+        Road(assets_manager.images['road'], 0, 0),
+        Road(assets_manager.images['road'], SCREEN_WIDTH, 0),
     )
     assets_manager.images['player'] = pygame.transform.scale(assets_manager.images['player'], (PLAYER_WIDTH, PLAYER_HEIGHT))
     player = Player(assets_manager.images['player'], SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
@@ -65,7 +67,7 @@ def main():
 
     running = True
     while running:
-        backgrounds.update(BACKGROUND_SPEED, 0)
+        roads.update(BACKGROUND_SPEED, 0)
         player.update(BACKGROUND_SPEED, 0)
 
         for event in pygame.event.get():
@@ -85,8 +87,7 @@ def main():
             player.dir = 0
         player.dir_change()
 
-
-        backgrounds.draw(window)
+        roads.draw(window)
         window.blit(player.image, player.rect)
         pygame.display.flip()
 
