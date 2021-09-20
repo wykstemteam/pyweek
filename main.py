2fad415aa1bc1398475891686d16ed0b82e74c46
Lestper <61917186+Lestper@users.noreply.github.com>
2021-09-20 21:28:58 +0800

    add background

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
+import math
 
+import numpy as np
 import pygame
+from skimage import io
+from skimage import transform
 
 from assets_manager import AssetsManager
 from constants import *
@@ -30,6 +33,45 @@ class Road(pygame.sprite.Sprite):
             self.rect.top -= self.height * 2
         elif self.rect.bottom < 0:
             self.rect.top += self.height * 2
+
+
+class Building(pygame.sprite.Sprite):
+    def shear(self, img: np.ndarray, bottom_shear_right: float):
+        img = img.astype('float32') / 255.0
+        img = img.swapaxes(0, 1)
+
+        affine_transform = transform.AffineTransform(shear=math.atan(bottom_shear_right / self.height))
+        modified_image = transform.warp(img, affine_transform) * 255.0
+        io.imshow(modified_image)
+        io.show()
+
+        modified_image = modified_image.swapaxes(0, 1)
+        return modified_image.astype('int32')
+
+    def __init__(self, image: pygame.Surface, bottom_shear_right: float, x: int, y: int):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.height = image.get_height()
+
+        pixels = np.dstack((
+            pygame.surfarray.pixels_red(image),
+            pygame.surfarray.pixels_green(image),
+            pygame.surfarray.pixels_blue(image),
+            pygame.surfarray.pixels_alpha(image),
+        )).astype('float32')
+        pixels /= 255.0
+        pixels = pixels.swapaxes(0, 1)
+        affine_transform = transform.AffineTransform(shear=math.atan(bottom_shear_right / self.height))
+
+        modified_image = transform.warp(pixels, affine_transform)
+        modified_image *= 255.0
+        modified_image = modified_image.astype('int32')
+        modified_image = modified_image.swapaxes(0, 1)
+
+        self.image = pygame.surfarray.make_surface(modified_image[:, :, :-1])
+        self.image.set_colorkey((0, 0, 0))  # black shits are transparent
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -108,7 +150,7 @@ def main():
                 player.acc(-PLAYER_ACC, 0)
         else:
             player.rect.left = SCREEN_WIDTH - PLAYER_WIDTH // 2
-        
+
         if player.rect.top <= 0:
             running = False
         elif player.rect.top >= SCREEN_HEIGHT - PLAYER_HEIGHT:
@@ -133,6 +175,7 @@ def main():
         window.blit(player.image, player.rect)
         window.blit(assets_manager.images['Darken'], pygame.Rect(0, 0, 0, 0))
         pygame.display.flip()
+
 
 if __name__ == '__main__':
     main()
