ef85ed11ca1cdf9ce260b117bbce5c99d5edbf38
Eason2123 <eason.ng23@gmail.com>
2021-09-21 23:17:53 +0800

    Quickfire (policecar)

--- game/sprites/policecar.py
+++ game/sprites/policecar.py
@@ -22,31 +22,54 @@ class PoliceCar(pygame.sprite.Sprite):
 
         self.objective = self.rect.centery
         self.velocity = POLICECAR_VELOCITY
+        self.state = 0
 
     def shoot(self, bullet_speed):
         self.bullets.add(Bullet(self.bullet_image, self.rect.center, bullet_speed))
 
     def update(self, t):
-        if self.rect.centery != self.objective:
-            if self.rect.centery > self.objective:
-                if abs(self.rect.centery - self.objective) < self.velocity * t:
-                    self.rect.centery = self.objective
+        if self.state == 0:  # bottom to top quickfire
+            while self.rect.centery != 50:
+                if self.rect.centery > 50:
+                    if abs(self.rect.centery - 50) < self.velocity * t:
+                        self.rect.centery = 50
+                    else:
+                        self.rect.centery -= self.velocity * t
                 else:
-                    self.rect.centery -= self.velocity * t
+                    if abs(self.rect.centery - 50) < self.velocity * t:
+                        self.rect.centery = 50
+                    else:
+                        self.rect.centery += self.velocity * t
+            while centery <= 400:
+                self.rect.centery += 100
+                self.bullets.update(t)
+                if self.shoot_cooldown <= 0:
+                    self.shoot(500)
+                    self.shoot_cooldown = QUICKFIRE_COOLDOWN
+                else:
+                    self.shoot_cooldown -= t
+            self.state = 1
+        else:
+            if self.rect.centery != self.objective:
+                if self.rect.centery > self.objective:
+                    if abs(self.rect.centery - self.objective) < self.velocity * t:
+                        self.rect.centery = self.objective
+                    else:
+                        self.rect.centery -= self.velocity * t
+                else:
+                    if abs(self.rect.centery - self.objective) < self.velocity * t:
+                        self.rect.centery = self.objective
+                    else:
+                        self.rect.centery += self.velocity * t
             else:
-                if abs(self.rect.centery - self.objective) < self.velocity * t:
-                    self.rect.centery = self.objective
-                else:
-                    self.rect.centery += self.velocity * t
-        else:
-            self.objective = random.randint(120, 600 - POLICECAR_HEIGHT)
+                self.objective = random.randint(120, 600 - POLICECAR_HEIGHT)
 
-        self.bullets.update(t)
-        if self.shoot_cooldown <= 0:
-            self.shoot(500)
-            self.shoot_cooldown = SHOOT_COOLDOWN
-        else:
-            self.shoot_cooldown -= t
+            self.bullets.update(t)
+            if self.shoot_cooldown <= 0:
+                self.shoot(500)
+                self.shoot_cooldown = SHOOT_COOLDOWN
+            else:
+                self.shoot_cooldown -= t
 
     def draw(self, window):
         window.blit(self.image, self.rect)
