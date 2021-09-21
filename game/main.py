51685be8b22635429cfd298a22d1351ec8ff3ebc
L3ungj <justin.leungj@gmail.com>
2021-09-22 00:37:46 +0800

    increased monitor size

--- game/main.py
+++ game/main.py
@@ -51,7 +51,10 @@ def gaming():
                     if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                         if event.ui_element == restart_button:
                             # init everything
-                            player.__init__()
+                            player.__init__(assets_manager.images['player'], SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+                            policecar.kill()
+                            policecar.__init__(assets_manager.images['policecar'], (20, 280), assets_manager.images['bullet'])
+                            obstacle_manager.kill()
                             lose = False
             lose_screen.process_events(event)
 
