9a0db9dbe1a44f2b87581efc27d315fc47e3ce6b
Sunny Cheung <sunnycheungabc@gmail.com>
2021-09-23 10:09:28 +0800

    shorten code and fix bug

--- game/sprites/laserbeam.py
+++ game/sprites/laserbeam.py
@@ -9,41 +9,24 @@ class Laser(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
 
-        self.image = pygame.Surface((SCREEN_WIDTH, 50), flags=pygame.SRCALPHA)
+        self.image = pygame.Surface((SCREEN_WIDTH, 50))
+        self.image.fill((127, 0, 0))
+        self.image.set_alpha(0)
         self.rect = self.image.get_rect()
-        self.image.fill((50, 0, 0, 0))
-        self.laser_cooldown = LASER_COOLDOWN
-        self.laserred_cooldown = LASERRED_COOLDOWN
-        self.laserblink_cooldown = LASERBLINK_COOLDOWN
-        self.laserremaintime = -1
-        self.laserrandom = False
+        self.last_laser_shoot = 0
+        self.t = 0
 
-    def update(self, t):
-        if self.laserrandom:
+    def update(self, dt):
+        self.t += dt
+        if self.t - self.last_laser_shoot >= LASER_COOLDOWN:
+            self.last_laser_shoot = self.t
             self.rect.topleft = (0, random.randint(BUILDING_HEIGHT, SCREEN_HEIGHT - LASER_HEIGHT))
-            self.laserrandom = False
-            self.laserremaintime = LASERREMAINTIME
 
-        if self.laser_cooldown <= 0:
-            self.laser_cooldown = 999
-            self.laserrandom = True
-        else:
-            self.laser_cooldown -= t
-
-        if self.laserremaintime >= 0:
-            self.laserremaintime -= t
-            if self.laserred_cooldown >= 0:
-                self.image.fill((127, 0, 0, 100))
-                self.laserred_cooldown -= t
-                self.laserblink_cooldown = LASERBLINK_COOLDOWN
+        if self.last_laser_shoot != 0 and self.t - self.last_laser_shoot <= LASERREMAINTIME:
+            if (self.t - self.last_laser_shoot) % (LASERBLINK_COOLDOWN * 2) < LASERBLINK_COOLDOWN:
+                self.image.set_alpha(127)
             else:
-                self.image.fill((127, 0, 0, 0))
-                self.laserblink_cooldown -= t
-                if self.laserblink_cooldown < 0:
-                    self.laserred_cooldown = LASERRED_COOLDOWN
-        elif self.laser_cooldown > 900:
-            self.image.fill((50, 0, 0, 0))
-            self.laser_cooldown = LASER_COOLDOWN
+                self.image.set_alpha(0)
 
     def draw(self, window):
         window.blit(self.image, self.rect)
