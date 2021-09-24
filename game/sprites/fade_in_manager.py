65d5463d436991d1f211200015eade5732046133
DGQSlol <dgqslol@gmail.com>
2021-09-24 21:24:18 +0800

    Revert "Merge branch 'master' of https://github.com/WYK-pyweek/pyweek"
    This reverts commit cc892a07c9c53c0356a4e6a4fd1795047f70f4bd, reversing
    changes made to ac554e3653907d09ddc1376a745950aa0508dbb4.

--- game/sprites/fade_in_manager.py
+++ game/sprites/fade_in_manager.py
@@ -22,9 +22,6 @@ class FadeInManager:
         if self.x < SCREEN_WIDTH:
             window.blit(self.gradient_strip, (self.x, 0))
             if self.x < 0:
-                window.fill(
-                    (0, 0, 0),
-                    pygame.Rect(self.x + SCREEN_WIDTH, 0, SCREEN_WIDTH - self.x, SCREEN_HEIGHT)
-                )
+                window.fill((0, 0, 0), pygame.Rect(self.x + SCREEN_WIDTH, 0, SCREEN_WIDTH - self.x, SCREEN_HEIGHT))
         else:
             self.fading_in = False
