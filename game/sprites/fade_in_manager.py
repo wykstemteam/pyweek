1aa48bf53496df326f3c639df58ee0d53c961fa4
Sunny Cheung <sunnycheungabc@gmail.com>
2021-09-24 20:02:39 +0800

    fade in manager

---
+++ game/sprites/fade_in_manager.py
@@ -0,0 +1,23 @@
+import pygame
+
+from game.constants import *
+
+
+class FadeInManager:
+    def __init__(self, gradient_strip: pygame.Surface):
+        self.gradient_strip = gradient_strip
+        self.x = -SCREEN_WIDTH
+        self.fading_in = False
+
+    def start_fade_in(self):
+        self.x = -SCREEN_WIDTH
+        self.fading_in = True
+
+    def update(self, t: float):
+        self.x += FADE_IN_VELOCITY * t
+
+    def draw(self, window: pygame.Surface):
+        if self.x < SCREEN_WIDTH:
+            window.blit(self.gradient_strip, (self.x, 0))
+        if self.x < 0:
+            window.fill((0, 0, 0), pygame.Rect(self.x + SCREEN_WIDTH, 0, SCREEN_WIDTH - self.x, SCREEN_HEIGHT))
