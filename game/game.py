8b2f65e0047c146c2f08186ab284def980cd926e
Lestper <61917186+Lestper@users.noreply.github.com>
2021-09-25 13:01:36 +0800

    Update game.py

--- game/game.py
+++ game/game.py
@@ -11,57 +11,30 @@ from game.sprites import *
 
 # TODO: Replace self.pause and self.lose with self.state which is GAMING, PAUSE or LOSE
 
-class scenes(Enum):
+class Scenes(Enum):
     CITY = 1
-    SPACE = 2
+    SHOP = 2
+    SPACE = 3
 
 class Game:
     def __init__(self) -> None:
-        cur_scene = scenes.CITY
+        self.cur_scene = Scenes.CITY
 
         # objects in all scenes
-        self.laser_manager = LaserManager(self.player_collision_group)
-        self.player_collision_group = pygame.sprite.Group()
-        self.arrow = Arrow(assets_manager.images['arrow'], self.player)
-
-        # objects in scene.city
-        self.roads = pygame.sprite.Group(
-            Road(assets_manager.images['road'], 0),
-            Road(assets_manager.images['road'], SCREEN_WIDTH),
-        )
-        self.policecar = PoliceCar(
-            assets_manager.images['policecar'], pygame.Vector2(20, 280),
-            assets_manager.images['bullet'], self.player_collision_group
-        )
-        self.bomber = Bomber(self.player_collision_group)
-        self.buildings = BuildingManager()
-
-        # objects in scene.space
-        self.spaceship = Spaceship(self.player_collision_group)
-        self.ufo = UFO(self.player_collision_group)
+        # ================================================================================================
         self.player = Player(
             assets_manager.images['motorbike'], SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, self
         )
-        self.obstacle_manager = ObstacleManager(self.player_collision_group)
+        self.player_collision_group = pygame.sprite.Group()
+        self.laser_manager = LaserManager(self.player_collision_group)
+        self.arrow = Arrow(assets_manager.images['arrow'], self.player)
         self.distance_manager = DistanceManager()
         self.screen_shake_manager = ScreenShakeManager()
         # self.screen_shake_manager.shaking = True
-
         self.fade_in_manager = FadeInManager(assets_manager.images['gradient_line'])
         self.fade_in_manager.start_fade_in()
-
         # Health_bar
         self.health_bar_image = assets_manager.images['HP4']
-
-        # shop
-        self.beach_image = assets_manager.images['beach_full']
-        self.shop = False
-        self.show_sea_time = SHOW_SEA_TIME
-        self.show_shop_animation = False
-        self.dimming = False
-        self.darken_alpha = 0
-        self.beach_rect = pygame.Rect(0, 0, 0, 0)
-
         # coins
         self.coin_manager = CoinManager(self.player_collision_group)
 
@@ -98,13 +71,46 @@ class Game:
         )
         self.lose = False
 
-        assets_manager.play_music("8bitaggressive1")
-
         # items
         self.bullet_time = False
         self.bullet_time_t = ITEM_BULLET_TIME_DURATION
+        self.rate = 1.0
         self.earthquake_time = ITEM_EARTHQUAKE_DURATION
         self.earthquake = False
+
+        # entering shop
+        self.shop = False
+        self.show_shop_animation = False
+        self.dimming = False
+        self.darken_alpha = 0
+        self.show_sea_time = SHOW_SEA_TIME
+
+        # objects in scene.CITY
+        # ================================================================================================
+        self.roads = pygame.sprite.Group(
+            Road(assets_manager.images['road'], 0),
+            Road(assets_manager.images['road'], SCREEN_WIDTH),
+        )
+        self.policecar = PoliceCar(
+            assets_manager.images['policecar'], pygame.Vector2(20, 280),
+            assets_manager.images['bullet'], self.player_collision_group
+        )
+        self.bomber = Bomber(self.player_collision_group)
+        self.buildings = BuildingManager()
+        self.obstacle_manager = ObstacleManager(self.player_collision_group)
+        self.beach_image = assets_manager.images['beach_full']
+        self.beach_rect = pygame.Rect(0, 0, 0, 0)
+
+        # objects in scene.SHOP
+        # ================================================================================================
+
+
+        # objects in scene.SPACE
+        # ================================================================================================
+        self.spaceship = Spaceship(self.player_collision_group)
+        self.ufo = UFO(self.player_collision_group)
+
+        assets_manager.play_music("8bitaggressive1")
 
     def event_process(self, window: pygame.Surface):
         for event in pygame.event.get():
@@ -128,114 +134,125 @@ class Game:
             self.lose_screen.process_events(event)
 
     def update(self, t: float) -> None:
-        self.player.hp = max(self.player.hp, 0)
-        self.health_bar_image = assets_manager.images[f"HP{self.player.hp}"]
 
-        if self.show_shop_animation:
-            self.arrow.rect.left += -BACKGROUND_VELOCITY * t
-            if self.player.go_right(t) and not self.dimming:
-                self.dimming = True
-            return
-
-        if self.bullet_time:
-            self.bullet_time_t -= t
-            if self.bullet_time_t <= 0:
-                self.bullet_time_t = ITEM_BULLET_TIME_DURATION
-                self.bullet_time = False
-            else:
-                rate = 0
-                if ITEM_BULLET_TIME_DURATION - self.bullet_time_t <= 1:
-                    rate = -math.sqrt(
-                        (ITEM_BULLET_TIME_DURATION - self.bullet_time_t) * (1 - BULLET_TIME_RATE)**2
-                    ) + 1
-                else:
-                    rate = (1 - BULLET_TIME_RATE) * (
-                        (1 - self.bullet_time_t / (ITEM_BULLET_TIME_DURATION - 1))**2
-                    ) + BULLET_TIME_RATE
-                t *= rate
-                print(rate, sep=' ')
-
+        # objects in all scenes:
         if not self.lose and not self.pause:
+            self.player.hp = max(self.player.hp, 0)
+            self.health_bar_image = assets_manager.images[f"HP{self.player.hp}"]
+            if self.show_shop_animation:
+                self.arrow.rect.left += -BACKGROUND_VELOCITY * t
+                if self.player.go_right(t) and not self.dimming:
+                    self.dimming = True
+                return
+
             self.fade_in_manager.update(t)
-            self.roads.update(t)
-            self.buildings.update(t, self.shop)
-            self.player.update(t if not self.bullet_time else t / rate)
-            self.policecar.update(t, self.shop)
-
-            # bomber
-            # if self.distance_manager.dist >= BOMBER_APPEAR_DIST-1 and self.distance_manager.dist <= BOMBER_APPEAR_DIST+1:
-            #    self.bomber.activated = True
-            # self.bomber.aim(self.player.rect.centerx, self.player.rect.centery)
-            # self.bomber.update(t)
-
-            # spaceship
-            # TODO: Hitbox for laser
-            # if self.distance_manager.dist >= 50 and self.distance_manager.dist <= 51:
-            #    self.spaceship.activated = True
-            # if self.distance_manager.dist >= 70 and self.distance_manager.dist <= 71:
-            #    self.spaceship.is_charge = True
-            # self.spaceship.update(t)
-
-            # UFO
-            if 50 <= self.distance_manager.dist <= 51:
-                self.ufo.activated = True
-            self.ufo.update(t)
-
-            self.obstacle_manager.update(t, self.shop)
+            self.player.update(t if not self.bullet_time else t / self.rate)
+            self.player_collision()
             self.coin_manager.update(t, self.shop)
             self.arrow.update(self.player)
             self.laser_manager.update(t, self.shop)
-            self.player_collision()
             if self.shop:
                 self.beach_rect.move_ip(BACKGROUND_VELOCITY // 100, 0)
                 self.show_sea_time -= t
                 if self.show_sea_time <= 0:
                     self.show_shop_animation = True
-            else: 
+            else:
+                if self.distance_manager.dist_to_next_country <= 0:
+                    self.trigger_shop()
                 self.distance_manager.update(t)
 
-        if self.player.hp <= 0 and not self.shop and not self.lose:
-            print(self.player.hp)
-            if not PLAYER_INVIN:
-                self.trigger_lose()
+            if self.player.hp <= 0:
+                print(self.player.hp)
+                if not PLAYER_INVIN:
+                    self.trigger_lose()
 
-        if self.distance_manager.dist_to_next_country <= 0 and not self.shop:
-            self.trigger_shop()
+            if self.earthquake:
+                self.earthquake_time -= t
+                if self.earthquake_time <= 0:
+                    self.earthquake = False
+                    self.screen_shake_manager.shaking = False
+                else:
+                    for obj in self.player_collision_group:
+                        if type(obj) not in (PoliceCar, Bomber, Spaceship, UFO, Coin):
+                            obj.kill()
 
+            if self.bullet_time:
+                self.bullet_time_t -= t
+                if self.bullet_time_t <= 0:
+                    self.bullet_time_t = ITEM_BULLET_TIME_DURATION
+                    self.bullet_time = False
+                else:
+                    self.rate = 0
+                    if ITEM_BULLET_TIME_DURATION - self.bullet_time_t <= 1:
+                        self.rate = -math.sqrt(
+                            (ITEM_BULLET_TIME_DURATION - self.bullet_time_t) * (1 - BULLET_TIME_RATE)**2
+                        ) + 1
+                    else:
+                        self.rate = (1 - BULLET_TIME_RATE) * (
+                            (1 - self.bullet_time_t / (ITEM_BULLET_TIME_DURATION - 1))**2
+                        ) + BULLET_TIME_RATE
+                    t *= self.rate
+                    print(self.rate, sep=' ')
+
+        # objects in scene.SHOP:
+        if self.cur_scene == Scenes.SHOP:
+            pass
+
+        # objects in scene.CITY:
+        if self.cur_scene == Scenes.CITY:
+            if not self.lose and not self.pause:
+                self.roads.update(t)
+                self.buildings.update(t, self.shop)
+                self.policecar.update(t, self.shop)
+                self.bomber.aim(self.player.rect.centerx, self.player.rect.centery)
+                self.bomber.update(t)
+                self.obstacle_manager.update(t, self.shop)
+
+        # objects in scene.SPACE:
+        if self.cur_scene == Scenes.SPACE:
+            if not self.lose and not self.pause:
+                self.spaceship.update(t)
+                self.ufo.update(t)
+
+        # gui
         self.game_screen.update(t)
         self.pause_screen.update(t)
         self.lose_screen.update(t)
 
-        if self.earthquake:
-            self.earthquake_time -= t
-            if self.earthquake_time <= 0:
-                self.earthquake = False
-                self.screen_shake_manager.shaking = False
-            else:
-                for obj in self.player_collision_group:
-                    if type(obj) not in (PoliceCar, Spaceship, Coin):
-                        obj.kill()
 
     def draw(self, window: pygame.Surface) -> None:
         window.fill((0, 0, 0))
-        self.roads.draw(window)
-        if self.shop:
-            window.blit(self.beach_image, self.beach_rect)
-        self.buildings.draw(window)
-        self.policecar.draw(window)
+
+        # objects in the ground:
+        if self.cur_scene == Scenes.CITY:
+            self.roads.draw(window)
+
+
+        # objects on the ground:
         self.laser_manager.draw(window)
-        self.obstacle_manager.draw(window)
         self.coin_manager.draw(window)
         self.player.draw(window)
+
+        if self.cur_scene == Scenes.CITY:
+            if self.shop:
+                window.blit(self.beach_image, self.beach_rect)
+            self.buildings.draw(window)
+            self.policecar.draw(window)
+            self.obstacle_manager.draw(window)
+
+
+        # objects that fly in the sky:
         self.arrow.draw(window)
-
-        # objects that fly
-        self.bomber.draw(window)
-        self.spaceship.draw(window)
-        self.ufo.draw(window)
         self.fade_in_manager.draw(window)
-
         self.distance_manager.draw(window)
+
+        if self.cur_scene == Scenes.CITY:
+            self.bomber.draw(window)
+
+        if self.cur_scene == Scenes.SPACE:
+            self.spaceship.draw(window)
+            self.ufo.draw(window)
+
 
         window.blit(self.health_bar_image, pygame.Rect((10, 10), (400, 100)))
         if self.pause:
