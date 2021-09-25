4932e267f4ad69cc2ebfe52a255fb68aef0b6ac0
Sunny Cheung <sunnycheungabc@gmail.com>
2021-09-25 18:24:00 +0800

    add shop

--- game/sprites/shop.py
+++ game/sprites/shop.py
@@ -51,9 +51,38 @@ class Shop:
             ) for i in range(6)
         ]
 
+        self.confirm_screen = pygame_gui.UIManager(
+            (SCREEN_WIDTH, SCREEN_HEIGHT), 'shop_confirm_theme.json'
+        )
+        self.display_buttons = [
+            pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect((678, 120), (145, 150)),
+                text='',
+                tool_tip_text="<font face=fira_code color=normal_text size=5>" 
+                              f"<b><u>{self.button_text[i]}</u></b>"
+                              "<br><br>"
+                              f"<i>{self.button_text_effect[i]}</i>",
+                object_id=f'#button_{i+1}',
+                manager=self.confirm_screen
+            ) for i in range(6)
+        ]
+        self._hide()
+
+        self.button_confirm = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((555, 301), (200, 60)),
+            text='Confirm',
+            manager=self.confirm_screen
+        )
+        self.button_cancel = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((755, 301), (200, 60)),
+            text='Cancel',
+            manager=self.confirm_screen
+        )
+
     def appear(self, window: pygame.Surface) -> None:
         clock = pygame.time.Clock()
         running = True
+        confirmation = False
 
         while running:
             t = clock.get_time()
@@ -69,63 +98,32 @@ class Shop:
                 elif event.type == pygame.USEREVENT:
                     if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                         button = event.ui_element
-                        self.confirm_menu(f'#button_{self.buttons.index(button) + 1}', window)
+                        if button in (self.button_cancel, self.button_confirm):
+                            self._hide()
+                            confirmation = False
+                        else:
+                            button_number = self.buttons.index(button)
+                            self.display_buttons[button_number].show()
+                            confirmation = True
 
                 self.shop_screen.process_events(event)
+                self.confirm_screen.process_events(event)
 
             self.shop_screen.update(t / 1000)
-            window.blit(self.main_menu, (0, 0))
+            self.confirm_screen.update(t / 1000)
+
+            window.blit(self.main_menu, (0, 0))  # draws background
             self.shop_screen.draw_ui(window)
+
+            if confirmation:  # have thing selected
+                window.blit(self.darken, (0, 0))
+                window.blit(self.confirm_button, (0, 0))
+                self.confirm_screen.draw_ui(window)
+
             pygame.display.flip()
 
             clock.tick(60)
 
-    def confirm_menu(self, button_name: str, window: pygame.Surface) -> None:
-        numbers = [int(word) for word in button_name.split("_") if word.isdigit()]
-        confirm_screen = pygame_gui.UIManager(
-            (SCREEN_WIDTH, SCREEN_HEIGHT), 'shop_confirm_theme.json'
-        )
-        button_display = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((678, 120), (145, 150)),
-            text="",
-            tool_tip_text="<font face=fira_code color=normal_text size=5>" ""
-                          f" <b><u>{self.button_text[numbers[0]-1]}</u></b>"
-                          "<br><br>"    
-                          f"<i>{self.button_text_effect[numbers[0]-1]}</i>",
-            object_id=button_name,
-            manager=confirm_screen
-        )
-        button_confirm = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((555, 301), (200, 60)),
-            text='Confirm',
-            manager=confirm_screen
-        )
-        button_cancel = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((755, 301), (200, 60)), text='Cancel', manager=confirm_screen
-        )
-
-        clock = pygame.time.Clock()
-        run = True
-        window.blit(self.darken, pygame.Rect(0, 0, 0, 0))
-
-        while run:
-            t = clock.get_time()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_p:
-                        run = False
-                elif event.type == pygame.USEREVENT:
-                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                        if event.ui_element in (button_confirm, button_cancel):
-                            run = False
-
-                confirm_screen.process_events(event)
-
-            window.blit(self.confirm_button, (0, 0))
-            confirm_screen.update(t / 1000)
-            confirm_screen.draw_ui(window)
-            pygame.display.flip()
-
-            clock.tick(60)
+    def _hide(self):
+        for button in self.display_buttons:
+            button.hide()
