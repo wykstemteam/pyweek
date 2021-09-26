0b0f0d029755ba7380c096c0b46f472df0afabeb
L3ungj <justin.leungj@gmail.com>
2021-09-26 17:59:43 +0800

    actual laser

--- game/sprites/comet.py
+++ game/sprites/comet.py
@@ -19,42 +19,61 @@ class Comet(pygame.sprite.Sprite):
         self.y += 3000 * -np.sin(self.dir)
 
         self.image = pygame.transform.rotate(
-            assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2 * np.pi)
+            assets_manager.images['comet'], ((
+                np.pi - self.dir) * 360) / (2 * np.pi)
         )
-        self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.x, self.y)).center)
+        self.rect = self.image.get_rect(
+            center=self.image.get_rect(center=(self.x, self.y)).center)
         self.hitbox = pygame.Rect(
-            (self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
+            (self.x - 15 + 70 * np.cos(self.dir),
+             self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
         )
         self.remaining_time = 30.0
         self.explode = None
         self.laser = Laser(self.rect.center, self.dir)
 
+    def hitbox_inbounds(self):
+        return (self.hitbox.bottom <= SCREEN_HEIGHT + 20 and self.hitbox.top >= -20 and
+                self.hitbox.right <= SCREEN_WIDTH + 20 and self.hitbox.left >= -20)
+
     def update(self, t: float, difficulty) -> None:
-        self.laser.update(t)
-        if self.laser.t > LASERREMAINTIME:
-            if not self.explode:
-                self.x += t * np.cos(self.dir) * 200 * min(5, difficulty)
-                self.y += t * np.sin(self.dir) * 200 * min(5, difficulty)
-                self.hitbox = pygame.Rect(
-                    (self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
-                )
-                self.image = pygame.transform.rotate(
-                    assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2 * np.pi)
-                )
-                self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.x, self.y)).center)
-                self.remaining_time -= t
-                if self.remaining_time <= 0:
+        if self.hitbox_inbounds():
+            self.laser.update(t)
+            if self.laser.t > LASERREMAINTIME:
+                if not self.explode:
+                    self.x += t * np.cos(self.dir) * 200 * min(5, difficulty)
+                    self.y += t * np.sin(self.dir) * 200 * min(5, difficulty)
+                    self.hitbox = pygame.Rect(
+                        (self.x - 15 + 70 * np.cos(self.dir),
+                        self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
+                    )
+                    self.image = pygame.transform.rotate(
+                        assets_manager.images['comet'], ((
+                            np.pi - self.dir) * 360) / (2 * np.pi)
+                    )
+                    self.rect = self.image.get_rect(
+                        center=self.image.get_rect(center=(self.x, self.y)).center)
+                    self.remaining_time -= t
+                    if self.remaining_time <= 0:
+                        self.kill()
+                elif not self.explode.update(t):
                     self.kill()
-            elif not self.explode.update(t):
-                self.kill()
+        else:
+            self.x += t * np.cos(self.dir) * 200 * min(5, difficulty)
+            self.y += t * np.sin(self.dir) * 200 * min(5, difficulty)
+            self.hitbox = pygame.Rect(
+                (self.x - 15 + 70 * np.cos(self.dir),
+                self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
+            )
 
     def draw(self, window: pygame.Surface) -> None:
-        self.laser.draw(window)
-        if self.image:
-            window.blit(self.image, self.rect)
-            # pygame.draw.rect(window, (255, 255, 255), self.hitbox)
-        elif self.explode and self.explode.image:
-            window.blit(self.explode.image, self.explode.rect)
+        if self.hitbox_inbounds():
+            self.laser.draw(window)
+            if self.image:
+                window.blit(self.image, self.rect)
+                # pygame.draw.rect(window, (255, 255, 255), self.hitbox)
+            elif self.explode and self.explode.image:
+                window.blit(self.explode.image, self.explode.rect)
 
     def collision_player(self, player):
         if player.rect.colliderect(self.hitbox):
@@ -62,4 +81,3 @@ class Comet(pygame.sprite.Sprite):
                 if player.hit():
                     self.image = None
                     self.explode = Explode(self.rect.center)
-        
--- game/sprites/laser.py
+++ game/sprites/laser.py
@@ -8,13 +8,18 @@ from game.constants import *
 class Laser(pygame.sprite.Sprite):
     def __init__(self, pos: Vector2, dir:float = 0) -> None:
         super().__init__()
-        self.image = pygame.Surface((SCREEN_WIDTH, 50), pygame.SRCALPHA)
+        self.image = pygame.Surface((SCREEN_WIDTH*2, 50), pygame.SRCALPHA)
         self.image.fill((127, 0, 0))
-        self.image = pygame.transform.rotate(self.image, dir * 180 / 3.14159)
+        self.image = pygame.transform.rotate(self.image, -dir * 180 / 3.14159)
         self.image.set_alpha(0)
         pos = Vector2(pos)
-        dx = SCREEN_WIDTH/2 - pos.x
-        dy = dx * np.tan(dir)
+        dx, dy = 0, 0
+        if abs(dir) >= 3.14159/2:
+            dx = SCREEN_WIDTH/2 - pos.x
+            dy = dx * np.tan(dir)
+        else:
+            dy = SCREEN_HEIGHT/2 - pos.y
+            dx = dy / np.tan(dir)
         self.pos = pos + Vector2(dx,dy) 
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
