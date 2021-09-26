3121beecc939c8bf4aa80810069f8f8be92cd42d
Jono <jowonowo@gmail.com>
2021-09-26 16:17:52 +0800

    Update comet.py

--- game/sprites/comet.py
+++ game/sprites/comet.py
@@ -1,6 +1,6 @@
-import numpy as np
 import random
 
+import numpy as np
 import pygame
 
 from game.assets_manager import assets_manager
@@ -16,21 +16,25 @@ class Comet(pygame.sprite.Sprite):
         self.x += 3000 * -np.cos(self.dir)
         self.y += 3000 * -np.sin(self.dir)
 
-        self.image = pygame.transform.rotate(assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2*np.pi))
-        self.rect = self.image.get_rect(
-            center=self.image.get_rect(center=(self.x, self.y)).center
+        self.image = pygame.transform.rotate(
+            assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2 * np.pi)
         )
-        self.hitbox = pygame.Rect((self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30))
+        self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.x, self.y)).center)
+        self.hitbox = pygame.Rect(
+            (self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
+        )
         self.remaining_time = 30.0
 
     def update(self, t: float, difficulty) -> None:
         self.x += t * np.cos(self.dir) * 200 * min(5, difficulty)
         self.y += t * np.sin(self.dir) * 200 * min(5, difficulty)
-        self.hitbox = pygame.Rect((self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30))
-        self.image = pygame.transform.rotate(assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2*np.pi))
-        self.rect = self.image.get_rect(
-            center=self.image.get_rect(center=(self.x, self.y)).center
+        self.hitbox = pygame.Rect(
+            (self.x - 15 + 70 * np.cos(self.dir), self.y - 15 + 70 * np.sin(self.dir)), (30, 30)
         )
+        self.image = pygame.transform.rotate(
+            assets_manager.images['comet'], ((np.pi - self.dir) * 360) / (2 * np.pi)
+        )
+        self.rect = self.image.get_rect(center=self.image.get_rect(center=(self.x, self.y)).center)
         self.remaining_time -= t
 
     def draw(self, window: pygame.Surface) -> None:
